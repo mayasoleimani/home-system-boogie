5213b0fbd838f3b2f68be994d6d287d20fa305f5
Maya Soleimani <mayasoleimani@gmail.com>
2026-05-27 19:34:05 -0700

    Updated netmiko operation

--- connect.py
+++ connect.py
@@ -1,16 +1,40 @@
 from netmiko import ConnectHandler
 
-router = {
-    'device_type': 'cisco_ios',
-    'host': '10.10.10.200',
-    'username': 'maya',
-    'password': 'cisco',
-    'global_cmd_verify': False,
-    'conn_timeout': 10,
-}
+routers = [
+    {
+        'device_type': 'cisco_ios',
+        'host': '10.10.10.200',
+        'username': 'maya',
+        'password': 'cisco',
+        'global_cmd_verify': False,
+    },
+    {
+        'device_type': 'cisco_ios',
+        'host': '10.10.11.201',
+        'username': 'elliot',
+        'password': 'cisco',
+        'global_cmd_verify': False,
+    },
+    {
+        'device_type': 'cisco_ios',
+        'host': '10.10.13.201',
+        'username': 'malliot',
+        'password': 'cisco',
+        'global_cmd_verify': False,
+    },
+]
 
-connection = ConnectHandler(**router)
-print("Connected successfully!")
-output = connection.send_command('show ip interface brief')
-print(output)
-connection.disconnect()
+for router in routers:
+    print(f"\nConnecting to {router['host']}...")
+    connection = ConnectHandler(**router)
+    
+    print("--- Device Inventory ---")
+    output = connection.send_command('show version')
+    print(output)
+    
+    print("--- Interface Summary ---")
+    output = connection.send_command('show ip interface brief')
+    print(output)
+    
+    connection.disconnect()
+    print(f"Disconnected from {router['host']}")
