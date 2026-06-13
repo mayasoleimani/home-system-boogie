3a7e2cc0854e780a48abda4ea18e6b67b0a75abe
Maya Soleimani <mayasoleimani@gmail.com>
2026-06-13 14:06:30 -0700

    added a nat gateway and private route table for private subnet internet access

--- phase2-aws/main.tf
+++ phase2-aws/main.tf
@@ -159,3 +159,41 @@ resource "aws_instance" "private" {
 output "bastion_public_ip" {
   value = aws_instance.bastion.public_ip
 }
+# Elastic IP for NAT Gateway
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "home-lab-nat-eip"
+  }
+}
+
+# NAT Gateway (in public subnet)
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public.id
+
+  tags = {
+    Name = "home-lab-nat"
+  }
+}
+
+# Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "home-lab-private-rt"
+  }
+}
+
+# Associate Private Subnet with Private Route Table
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
